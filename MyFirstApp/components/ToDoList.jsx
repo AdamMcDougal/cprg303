d1c5d783c64d32f17246fd8f1d4cf615b24ff087
AMcdougal <31479662+AdamMcDougal@users.noreply.github.com>
2024-03-01 13:02:23 -0700

    Lab 3

--- MyFirstApp/components/ToDoList.jsx
+++ MyFirstApp/components/ToDoList.jsx
@@ -10,28 +10,20 @@ import {
     Button
   } from 'react-native';
   
-export default function ToDoList(props) {
-  return (
-    <>
-      <ScrollView>
-        <Pressable>
-          <View style={[styles.task, styles.completed]}>
-            <Text style={styles.taskText}>Do laundry</Text>
-          </View>
-        </Pressable>
-        <Pressable>
-          <View style={[styles.task]}>
-            <Text style={styles.taskText}>Go to gym</Text>
-          </View>
-        </Pressable>
-        <Pressable>
-          <View style={[styles.task, styles.completed]}>
-            <Text style={styles.taskText}>Walk dog</Text>
-          </View>
-        </Pressable>
-      </ScrollView>
-    </>
-  );
+export default function ToDoList({ tasks }) {
+	
+
+	return (
+		<ScrollView>
+			{tasks.map((task, key) => (
+				<Pressable key={key}>
+					<View style={[styles.task]}>
+						<Text style={styles.taskText}>{task}</Text>
+					</View>
+				</Pressable>
+			))}
+		</ScrollView>
+	);
 }
 
 const styles = StyleSheet.create({
